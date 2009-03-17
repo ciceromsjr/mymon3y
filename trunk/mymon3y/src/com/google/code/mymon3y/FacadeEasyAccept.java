c9fc87e616c6ad877f045ac3d35a33bf6194dc44
jaindsonvs@gmail.com <jaindsonvs@gmail.com@691cee16-11ac-11de-af30-3d3b2b6d83b0>
2009-03-17 04:26:27 +0000

    Commit da primeira versão da estrutura do projeto e da Facade do EasyAccept, além dos testes de aceitação da User Story 1.

---
+++ trunk/mymon3y/src/com/google/code/mymon3y/FacadeEasyAccept.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2009
+ * 
+ * Jaindson Valentim Santana (jaindsonvs [at] gmail [dot] com)
+ * Matheus Gaudencio do Rêgo (matheusgr [at] gmail [dot] com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+package com.google.code.mymon3y;
+
+/**
+ * @author Jaindson Valentim Santana
+ * @author Matheus Gaudencio do Rêgo
+ * 
+ */
+public class FacadeEasyAccept {
+
+	public void zerarSistema() {
+		// TODO
+	}
+
+	public void criarUsuario(String login, String senha) {
+		// TODO
+	}
+
+	public void editarAtributoUsuario(String login, String atributo, String valor) {
+		// TODO
+	}
+
+	public String getAtributoUsuario(String login, String atributo) {
+		// TODO
+		return null;
+	}
+}
