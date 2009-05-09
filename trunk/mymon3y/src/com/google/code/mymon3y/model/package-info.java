1050b42867b45da04675dd364908b4ffc80f3fa4
jaindsonvs@gmail.com <jaindsonvs@gmail.com@691cee16-11ac-11de-af30-3d3b2b6d83b0>
2009-05-09 19:43:17 +0000

    Comentários dos pacotes.

---
+++ trunk/mymon3y/src/com/google/code/mymon3y/model/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Todas as classes relacionadas ao modelo lógico estão neste pacote.
+ */
+package com.google.code.mymon3y.model;
---
+++ trunk/mymon3y/src/com/google/code/mymon3y/persistencia/dao/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Todas as classes relacionadas ao padrão <i>Data Access Object</i> (DAO) estão neste pacote.
+ */
+package com.google.code.mymon3y.persistencia.dao;
