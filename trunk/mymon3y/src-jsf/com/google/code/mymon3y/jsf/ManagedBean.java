d9af0408a15418231f6f6cefd93649fb33ad7a90
jaindsonvs@gmail.com <jaindsonvs@gmail.com@691cee16-11ac-11de-af30-3d3b2b6d83b0>
2009-06-01 03:10:51 +0000

    Camada Web - Autenticação de acesso (usuário deve estar logado para acessar algumas funcionalidades do sistema) - DONE

--- trunk/mymon3y/src-jsf/com/google/code/mymon3y/jsf/ManagedBean.java
+++ trunk/mymon3y/src-jsf/com/google/code/mymon3y/jsf/ManagedBean.java
@@ -42,7 +42,7 @@ public class ManagedBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private static final String ID_USUARIO = "ID_USUARIO";
+	public static final String ID_USUARIO = "ID_USUARIO";
 	
 	public ManagedBean() {
 	}
---
+++ trunk/mymon3y/web2/erro.jsp
@@ -0,0 +1,7 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head></head>
+	<body>
+		<jsp:forward page="erro.jsf" />
+	</body>
+</html>
